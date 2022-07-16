f05e81acb437b04078fa749d32de664ce26b6387
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-16 11:47:59 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_integration/controller/auth_controller.dart';
 import 'package:firebase_integration/screens/welcome_screend.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
   runApp(const MyApp());
 }
 
@@ -10,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
     );
