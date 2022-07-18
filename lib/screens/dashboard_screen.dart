ff443b4c8273ffba6bb75994090389e9eb6e1c6d
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-18 23:21:09 +0700

    Update dashboard_screen.dart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_integration/controller/auth_controller.dart';
 import 'package:firebase_integration/screens/add_product.dart';
 import 'package:firebase_integration/utils/appbar_login.dart';
-import 'package:firebase_integration/utils/custom_button.dart';
 import 'package:firebase_integration/utils/product_test.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -17,60 +16,56 @@ class DashboardScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const LoginAppBar(
+              LoginAppBar(
                 title: 'Explore products',
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    ProductTest(),
-                    ProductTest(),
-                    ProductTest(),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    ProductTest(),
-                    ProductTest(),
-                    ProductTest(),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    ProductTest(),
-                    ProductTest(),
-                    ProductTest(),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    ProductTest(),
-                    ProductTest(),
-                    ProductTest(),
-                  ],
-                ),
-              ),
-              CustomButton(
-                onTap: () {
+                onPress: () {
                   authController.logout();
                 },
-                text: "LogOut",
-                textStyle: const TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.white),
+                icon: Icons.logout_outlined,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    ProductTest(),
+                    ProductTest(),
+                    ProductTest(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    ProductTest(),
+                    ProductTest(),
+                    ProductTest(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    ProductTest(),
+                    ProductTest(),
+                    ProductTest(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    ProductTest(),
+                    ProductTest(),
+                    ProductTest(),
+                  ],
+                ),
               ),
             ],
           ),
