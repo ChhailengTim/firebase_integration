153ebcbb0a7476f4bb6bd22fc253e31c2b91bb7e
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-18 23:01:58 +0700

    Update add_product.dart

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -18,78 +18,80 @@ class AddProduct extends StatelessWidget {
         title: const Text("Adding product"),
       ),
       body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  onSaved: (value) {
-                    productController.proname(value);
-                  },
-                  controller: productController.proName.value,
-                  label: "Product name",
-                ),
-                CustomTextFormField(
-                  onSaved: (value) {
-                    productController.procategory(value);
-                  },
-                  controller: productController.proCategory.value,
-                  label: "Category",
-                ),
-                CustomTextFormField(
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly, //cant input text
-                  ],
-                  onSaved: (value) {
-                    productController.proprice(value);
-                  },
-                  onChanged: (value) {
-                    productController.proprice(value);
-                    productController.priceCalculator();
-                  },
-                  controller: productController.proPrice.value,
-                  label: "Price",
-                ),
-                CustomTextFormField(
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly, //cant input text
-                    LengthLimitingTextInputFormatter(3) //only 3 number
-                  ],
-                  onSaved: (value) {
-                    productController.prodiscout(value);
-                  },
-                  onChanged: (value) {
-                    productController.prodiscout(value);
-                    productController.priceCalculator();
-                  },
-                  controller: productController.proDiscout.value,
-                  label: "Discount",
-                ),
-                CustomTextFormField(
-                  enable: false,
-                  controller: productController.proAmout.value,
-                  hintText: 'Total Amount',
-                  label: "Amount",
-                ),
-                CustomButton(
-                  onTap: () {
-                    _formKey.currentState!.save();
-                    productController
-                        .addProduct()
-                        .then((value) => Navigator.pop(context));
-                    Get.snackbar('Success', 'Product Add successfully',
-                        backgroundColor: Colors.green);
-                  },
-                  text: "Add product",
-                  textStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 15,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  CustomTextFormField(
+                    onSaved: (value) {
+                      productController.proname(value);
+                    },
+                    controller: productController.proName.value,
+                    label: "Product name",
                   ),
-                ),
-              ],
+                  CustomTextFormField(
+                    onSaved: (value) {
+                      productController.procategory(value);
+                    },
+                    controller: productController.proCategory.value,
+                    label: "Category",
+                  ),
+                  CustomTextFormField(
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly, //cant input text
+                    ],
+                    onSaved: (value) {
+                      productController.proprice(value);
+                    },
+                    onChanged: (value) {
+                      productController.proprice(value);
+                      productController.priceCalculator();
+                    },
+                    controller: productController.proPrice.value,
+                    label: "Price",
+                  ),
+                  CustomTextFormField(
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly, //cant input text
+                      LengthLimitingTextInputFormatter(3) //only 3 number
+                    ],
+                    onSaved: (value) {
+                      productController.prodiscout(value);
+                    },
+                    onChanged: (value) {
+                      productController.prodiscout(value);
+                      productController.priceCalculator();
+                    },
+                    controller: productController.proDiscout.value,
+                    label: "Discount",
+                  ),
+                  CustomTextFormField(
+                    enable: false,
+                    controller: productController.proAmout.value,
+                    hintText: 'Total Amount',
+                    label: "Amount",
+                  ),
+                  CustomButton(
+                    onTap: () {
+                      _formKey.currentState!.save();
+                      productController
+                          .addProduct()
+                          .then((value) => Navigator.pop(context));
+                      Get.snackbar('Success', 'Product Add successfully',
+                          backgroundColor: Colors.purple);
+                    },
+                    text: "Add product",
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
