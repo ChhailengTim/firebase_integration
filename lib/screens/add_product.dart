69e5805de41a2a70e01c377dd4d1147db056b3c1
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-19 23:45:02 +0700

    Update add_product.dart

--- lib/screens/add_product.dart
+++ lib/screens/add_product.dart
@@ -1,17 +1,64 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_integration/controller/product_controller.dart';
+import 'package:firebase_integration/model/product_model.dart';
 import 'package:firebase_integration/utils/custom_button.dart';
 import 'package:firebase_integration/utils/custom_textformfield.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
-class AddProduct extends StatelessWidget {
-  const AddProduct({Key? key}) : super(key: key);
+class AddProduct extends StatefulWidget {
+  AddProduct({
+    Key? key,
+    this.productID,
+  }) : super(key: key);
   static final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String? productID;
+
+  @override
+  State<AddProduct> createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  final ProductController productController = Get.put(ProductController());
+  File? image;
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+      if (image == null) return;
+      final imageTemp = File(image.path);
+
+      setState(() {
+        this.image = imageTemp;
+      });
+    } on PlatformException catch (e) {
+      print('Failed pick image: $e');
+    }
+  }
+
+  Future uploadImage() async {
+    final postID = DateTime.now().millisecondsSinceEpoch.toString();
+    var item = ProductModel();
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        .child("${widget.productID}/photo")
+        .child('pro_$postID');
+
+    await ref.putFile(image!);
+
+    //upload to cloudfirestore
+    firebaseFirestore.collection('products').doc(item.name).collection('photo');
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ProductController productController = Get.put(ProductController());
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
@@ -20,18 +67,32 @@ class AddProduct extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Form(
-            key: _formKey,
+            key: AddProduct._formKey,
             child: Padding(
               padding: const EdgeInsets.all(12.0),
               child: Column(
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black38),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: Icon(Icons.add),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  image != null
+                      ? Image.file(image!)
+                      : const Text('No image selected'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      pickImage();
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black38),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Icon(Icons.add),
+                      ),
                     ),
                   ),
                   CustomTextFormField(
@@ -85,7 +146,8 @@ class AddProduct extends StatelessWidget {
                   ),
                   CustomButton(
                     onTap: () {
-                      _formKey.currentState!.save();
+                      uploadImage();
+                      AddProduct._formKey.currentState!.save();
                       productController
                           .addProduct()
                           .then((value) => Navigator.pop(context));
