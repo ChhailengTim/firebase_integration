ac9ed29c8ba83634bfa30a13bc228dfaf37f8221
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-07-28 22:45:35 +0700

    Create resturant_appbar.dart

---
+++ lib/utils/resturant_appbar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ResturantAppBar extends StatelessWidget {
+  const ResturantAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Image.network(
+          "https://www.siemreap.net/wp-content/uploads/2018/07/Charcoal-Khmer-Food.jpg",
+          fit: BoxFit.cover,
+        ),
+      ),
+      leading: const Padding(
+        padding: EdgeInsets.only(left: 16),
+        child: CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Icon(Icons.arrow_back),
+        ),
+      ),
+      actions: const [
+        CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Icon(Icons.share),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Icon(Icons.search),
+          ),
+        ),
+      ],
+    );
+  }
+}
